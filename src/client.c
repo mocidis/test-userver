665dfe8e334f8fe12f638227a394704f413af1e9
buidanhquy <quy.buidanhquy@gmail.com>
2015-09-09 09:55:17 +0700

    do merge

--- src/client.c
+++ src/client.c
@@ -11,24 +11,18 @@ void usage(char *app) {
 
 
 int main(int argc, char *argv[]) {
-    char buffer[50];
 	myproto_client_t uclient;
-	char *server = argv[1];
     myproto_request_t req;
 
 	if( argc < 2 ) {
 		usage(argv[0]);
 	}
 	myproto_client_open(&uclient, argv[1]);
-    strncpy(req.arbiter_update.name, "HOANG XUAN TUNG", sizeof(req.arbiter_update.name));
+
     
-    printf("-- %s\n", req.arbiter_update.name);
-
-    req.msg_id = UUU_SHIT;
-    req.uuu_shit.yyy = 38;
-    req.uuu_shit.xxx = 0.8;
-
-    printf("%d, %d, %lf\n", req.msg_id, req.uuu_shit.yyy, req.uuu_shit.xxx);
+    req.msg_id = ARBITER_UPDATE;
+    strncpy(req.arbiter_update.name, "HOANG XUAN TUNG", sizeof(req.arbiter_update.name));
+    req.arbiter_update.age = 38;
 
 	myproto_client_send(&uclient, &req);
 
--- src/server.c
+++ src/server.c
@@ -14,11 +14,14 @@ static void on_request(myproto_server_t *userver, myproto_request_t *request) {
     switch(request->msg_id) {
     case ARBITER_UPDATE:
         printf("arbiter update\n");
+        printf("name:%s, age:%d\n", request->arbiter_update.name, request->arbiter_update.age);
         break;
     case ARBITER_REFRESH:
         printf("arbiter refresh\n");
+        printf("address: %s, weight:%lf\n", request->arbiter_refresh.address, request->arbiter_refresh.weight);
         break;
     case UUU_SHIT:
+        printf("uuu shit\n");
         printf("yyy:%d, xxx:%lf\n", request->uuu_shit.yyy, request->uuu_shit.xxx);
         break;
     }
@@ -32,7 +35,7 @@ int main(int argc, char *argv[]) {
 
 	myproto_server_init(&userver, argv[1]);
 	
-	userver.on_request = &on_request;
+	userver.on_request_f = &on_request;
 
 	myproto_server_start(&userver);
 	// Main loop goes here
